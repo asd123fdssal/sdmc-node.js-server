034183df1457d93597e5858165cc912afcac5c89
ARUSD <asd123fdssal@naver.com>
2023-04-19 21:34:11 +0900

    #13 게임 디테일 기능 구현중

--- config/sql_query.js
+++ config/sql_query.js
@@ -1,7 +1,6 @@
 const {MAX_CONTENTS} = require('./const_config');
 
-function get_game_title(page){
-    const offset =((page - 1) * MAX_CONTENTS);
+function get_game_title(){
     return `
         select
             g.img_dir as \`title_picture\`, 
@@ -15,35 +14,28 @@ function get_game_title(page){
         left join game_vote v on v.games_id = g.id
         group by g.id
         order by g.id desc
-        limit ${offset}, ${MAX_CONTENTS};
+        limit ?, ?;
     `
 }
 
-function get_login_result(username, password){
-    return (`select id as \`uid\` from member where username = \'${username}\' and password = \'${password}\' limit 1`);
+function get_login_result(){
+    return (`select id as \`uid\` from member where username = ? and password = ? limit 1`);
 }
 
-function sign_up_member(username, pw, email, nickname){
-    let verify_code = '';
-    for (let i = 0; i < 6; i++) {
-        verify_code += Math.floor(Math.random() * 10);
-    }
-
-    const verify_date = new Date(new Date().getTime() + 30 * 60 * 1000).toISOString().slice(0, 19).replace('T', ' ');
-
-    return (`insert into member values(null, \'${username}\',   \'${pw}\',    \'${email}\',   \'${nickname}\',   0,    \'${verify_code}\',  \'${verify_date}\'  );`)
+function sign_up_member(){
+    return (`insert into member values(null, ?, ?, ?, ?, 0, ?, ?);`)
 }
 
-function isDuplicateUsername(username){
-    return (`select username from member where username = \'${username}\'`);
+function isDuplicateUsername(){
+    return (`select username from member where username = ?`);
 }
 
-function isDuplicateEmail(email){
-    return (`select mail from member where mail = \'${email}\'`);
+function isDuplicateEmail(){
+    return (`select mail from member where mail = ?`);
 }
 
-function isDuplicateNickname(nickname){
-    return (`select nickname from member where nickname = \'${nickname}\'`);
+function isDuplicateNickname(){
+    return (`select nickname from member where nickname = ?`);
 }
 
 function isDuplicateGame(kor_name, release_date){
@@ -114,6 +106,21 @@ function insertGameShop(){
     return (`insert into game_member_has_shop (id, game_shop_id, member_id, games_id) values(null, (select id from game_shop where kor_name = ?) , ?, (select id from games where kor_name = ?))`)
 }
 
+function selectGameByID(){
+    return (`select c.kor_name as \`company\`, s.kor_name as \`series\`, g.img_dir, g.release_date, g.org_name, g.kor_name, g.synopsis, g.hookcode, g.etc, g.nickname from games g left join game_company c on c.id = g.company_id left join game_series s on s.id = g.series_id where g.id = ?`);
+}
+
+function selectGameGenreByGid() {
+    return (`select kor_name as \'value\' from game_has_genre ghg inner join game_genre g on g.id = ghg.genre_id where ghg.games_id = ?`);
+}
+
+function selectMyGameShop(){
+    return (`select kor_name from game_member_has_shop mhs inner join member m on m.id = mhs.member_id inner join game_shop gs on gs.id = mhs.game_shop_id where mhs.games_id = ? and mhs.member_id = ?`);
+}
+
+function selectCharcterByGid(){
+    return (`SELECT * FROM characters where games_id = ?`);
+}
 
 module.exports = {
     get_game_title,
@@ -139,4 +146,8 @@ module.exports = {
     insertNewGameShop,
     insertGameGenre,
     insertGameShop,
+    selectGameByID,
+    selectGameGenreByGid,
+    selectMyGameShop,
+    selectCharcterByGid,
 }
